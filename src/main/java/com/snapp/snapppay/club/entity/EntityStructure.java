efa88e61514e06a92eefc203f78922280544efa9
Golabchi <alireza.golabchi@gmail.com>
2024-10-13 20:17:11 +0330

    add EntityStructure add User add UserRole add Role

---
+++ src/main/java/com/snapp/snapppay/club/entity/EntityStructure.java
@@ -0,0 +1,41 @@
+package com.snapp.snapppay.club.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.sql.Timestamp;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Data
+public class EntityStructure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @CreatedBy
+    @Column(name = "insert_user")
+    private Long insertUser;
+
+    @LastModifiedBy
+    @Column(name = "update_user")
+    private Long updateUser;
+
+    @CreatedDate
+    @Column(name = "insert_date_time")
+    private Timestamp insertDateTime;
+
+    @LastModifiedDate
+    @Column(name = "update_date_time")
+    private Timestamp updateDateTime;
+
+    @Version
+    private Long version;
+
+}
---
+++ src/main/java/com/snapp/snapppay/club/entity/User.java
@@ -0,0 +1,32 @@
+package com.snapp.snapppay.club.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity(name = "user")
+@Table(
+        name = "users",
+        indexes = {
+                @Index(name = "idx_username", columnList = "username"),
+                @Index(name = "idx_national_code", columnList = "national_code")
+        })
+public class User extends EntityStructure {
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+    @Column(name = "password", nullable = false)
+    private String password;
+    @Column(name = "national_code", nullable = false, unique = true)
+    private String nationalCode;
+    @Column(nullable = false)
+    private Boolean active = true;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Set<UserRole> roles = new HashSet<>();
+}
