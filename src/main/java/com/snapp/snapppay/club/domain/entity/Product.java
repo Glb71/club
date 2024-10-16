8ba700e3683062d2b4da93baa6c4cb6259667652
Golabchi <alireza.golabchi@gmail.com>
2024-10-16 15:26:54 +0330

    add Product

---
+++ src/main/java/com/snapp/snapppay/club/domain/entity/Product.java
@@ -0,0 +1,26 @@
+package com.snapp.snapppay.club.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Entity(name = "product")
+@Table(name = "product")
+public class Product extends EntityStructure {
+
+    @Column(unique = true, nullable = false)
+    private String title;
+    @Lob
+    private String description;
+    private String image;
+    @Column(nullable = false)
+    private Boolean active = true;
+    @Column(name = "score_price", nullable = false)
+    private Integer scorePrice;
+
+}
