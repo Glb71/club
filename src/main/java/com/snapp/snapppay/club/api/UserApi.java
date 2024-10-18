81682ed01fd3e2486ad2fd30b872462cd9f543bd
Golabchi <alireza.golabchi@gmail.com>
2024-10-18 11:03:33 +0330

    add swagger documentation for UserApi

--- src/main/java/com/snapp/snapppay/club/api/UserApi.java
+++ src/main/java/com/snapp/snapppay/club/api/UserApi.java
@@ -8,6 +8,12 @@ import com.snapp.snapppay.club.mapper.UserResponseMapper;
 import com.snapp.snapppay.club.mapper.UserScoreResponseMapper;
 import com.snapp.snapppay.club.service.user.UserService;
 import com.snapp.snapppay.club.service.userScore.UserScoreLoader;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -20,6 +26,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@Tag(
+        name = "User Api",
+        description = "search, score"
+)
+
 public class UserApi {
 
     private final UserService userService;
@@ -29,12 +40,45 @@ public class UserApi {
 
     @GetMapping("/search")
     @PreAuthorize("hasAuthority('ADMIN')")
-    public Page<UserResponse> search(@RequestParam(value = "search", required = false) String search, @Valid PageRequest pageRequest) {
+    @Operation(
+            summary = "search",
+            description = "search for users (admin)"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "search successful",
+                            content = {@Content(mediaType = "application/json")}),
+                    @ApiResponse(responseCode = "401", description = "expired token",
+                            content = {@Content(mediaType = "text/plain")}),
+                    @ApiResponse(responseCode = "403", description = "Access Denied",
+                            content = {@Content(mediaType = "text/plain")}),
+                    @ApiResponse(responseCode = "400", description = "invalid input (missing page)",
+                            content = {@Content(mediaType = "application/json")})
+            }
+    )
+    public Page<UserResponse> search(
+            @Parameter(description = "search parameter (username)")
+            @RequestParam(value = "search", required = false) String search,
+            @Valid PageRequest pageRequest) {
         Page<User> users = userService.search(search, pageRequest);
         return users.map(userResponseMapper::map);
     }
 
     @GetMapping("/score")
+    @Operation(
+            summary = "get current user score",
+            description = "gives current user score and if its not created returns a new one"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "successful",
+                            content = {@Content(mediaType = "application/json")}),
+                    @ApiResponse(responseCode = "401", description = "expired token",
+                            content = {@Content(mediaType = "text/plain")}),
+                    @ApiResponse(responseCode = "403", description = "Access Denied",
+                            content = {@Content(mediaType = "text/plain")})
+            }
+    )
     public UserScoreResponse getUserScore() {
         return userScoreResponseMapper.map(userScoreLoader.current());
     }
