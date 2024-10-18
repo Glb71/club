5e4ed32e8f5394688d5ab31b8499c5126f348713
Golabchi <alireza.golabchi@gmail.com>
2024-10-18 15:50:12 +0330

    add integration test for AuthApi

--- src/test/java/com/snapp/snapppay/club/api/AuthApiIT.java
+++ src/test/java/com/snapp/snapppay/club/api/AuthApiIT.java
@@ -6,6 +6,7 @@ import com.snapp.snapppay.club.domain.entity.UserRole;
 import com.snapp.snapppay.club.domain.request.LoginRequest;
 import com.snapp.snapppay.club.domain.request.UserRegisterRequest;
 import com.snapp.snapppay.club.enums.Roles;
+import com.snapp.snapppay.club.exception.ExceptionMessageCode;
 import com.snapp.snapppay.club.repository.UserRepository;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.*;
@@ -16,10 +17,12 @@ import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import java.util.Collections;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -56,8 +59,7 @@ class AuthApiIT {
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setUsername("user");
         loginRequest.setPassword("userTestPassword");
-        mockMvc.perform(post("/api/auth/login").contentType(MediaType.APPLICATION_JSON).content(
-                objectMapper.writeValueAsString(loginRequest))).andExpect(status().isOk());
+        mockMvc.perform(buildLoginRequest(loginRequest)).andExpect(status().isOk());
     }
 
     @Test
@@ -66,8 +68,7 @@ class AuthApiIT {
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setUsername("user");
         loginRequest.setPassword("invalidPassword");
-        mockMvc.perform(post("/api/auth/login").contentType(MediaType.APPLICATION_JSON).content(
-                objectMapper.writeValueAsString(loginRequest))).andExpect(status().isUnauthorized());
+        mockMvc.perform(buildLoginRequest(loginRequest)).andExpect(status().isUnauthorized());
     }
 
     @Test
@@ -77,8 +78,43 @@ class AuthApiIT {
         userRegisterRequest.setUsername("user1");
         userRegisterRequest.setPassword("userTestPassword");
         userRegisterRequest.setNationalCode("1234567812");
-        mockMvc.perform(post("/api/auth/register").contentType(MediaType.APPLICATION_JSON).content(
-                objectMapper.writeValueAsString(userRegisterRequest))).andExpect(status().isOk());
+        mockMvc.perform(buildRegisterRequest(userRegisterRequest)).andExpect(status().isOk());
+    }
+
+    @Test
+    @SneakyThrows
+    void testDuplicateUsernameError() {
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setUsername("user");
+        userRegisterRequest.setPassword("userTestPassword");
+        userRegisterRequest.setNationalCode("1234567812");
+        mockMvc.perform(buildRegisterRequest(userRegisterRequest)).andExpect(status().isBadRequest()).
+                andExpect(content().string(ExceptionMessageCode.USERNAME_IS_DUPLICATE));
+    }
+
+    @Test
+    @SneakyThrows
+    void testDuplicateNationalCodeError() {
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setUsername("user1");
+        userRegisterRequest.setPassword("userTestPassword");
+        userRegisterRequest.setNationalCode("1111111111");
+        mockMvc.perform(buildRegisterRequest(userRegisterRequest)).andExpect(status().isBadRequest()).
+                andExpect(content().string(ExceptionMessageCode.NATIONAL_CODE_IS_DUPLICATE));
+    }
+
+    MockHttpServletRequestBuilder buildLoginRequest(LoginRequest request) {
+        return buildPostRequest("/api/auth/login", request);
+    }
+
+    MockHttpServletRequestBuilder buildRegisterRequest(UserRegisterRequest request) {
+        return buildPostRequest("/api/auth/register", request);
+    }
+
+    @SneakyThrows
+    MockHttpServletRequestBuilder buildPostRequest(String url, Object body) {
+        return post(url).contentType(MediaType.APPLICATION_JSON).content(
+                objectMapper.writeValueAsString(body));
     }
 
     @AfterEach
