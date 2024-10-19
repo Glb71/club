aacb667973837c3670a2778882a087f433ad2530
Golabchi <alireza.golabchi@gmail.com>
2024-10-19 22:43:42 +0330

    fix UserProductResponseMapper

--- src/main/java/com/snapp/snapppay/club/mapper/UserProductResponseMapper.java
+++ src/main/java/com/snapp/snapppay/club/mapper/UserProductResponseMapper.java
@@ -3,10 +3,12 @@ package com.snapp.snapppay.club.mapper;
 import com.snapp.snapppay.club.domain.entity.Product;
 import com.snapp.snapppay.club.domain.response.UserProductResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserProductResponseMapper {
 
+    @Mapping(source = "scorePrice",target = "scoreAmount")
     UserProductResponse map(Product product);
 
 }
