327be8e24d96eba7ad41bbafad497677178e3f1b
Golabchi <alireza.golabchi@gmail.com>
2024-10-19 19:00:31 +0330

    add Test for ProviderApi (missing change)

--- src/main/java/com/snapp/snapppay/club/domain/request/AddScoreRequest.java
+++ src/main/java/com/snapp/snapppay/club/domain/request/AddScoreRequest.java
@@ -4,10 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Schema(description = "request to add score")
+@Builder
 public class AddScoreRequest {
     @Schema(description = "score will be added to this username")
     @NotBlank
